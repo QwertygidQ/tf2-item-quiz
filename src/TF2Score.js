c300707440e428a708708a2081383992a1d8205a
QwertygidQ <Qwertygid@gmail.com>
2018-11-18 03:33:15 +0300

    New +1 animation

--- src/TF2Score.js
+++ src/TF2Score.js
@@ -1,14 +1,29 @@
 import React, { Component } from 'react';
+import { CSSTransition } from 'react-transition-group';
+
 import './css/TF2Item.css';
+import './css/Animations.css';
 
 class TF2Score extends Component {
+    constructor(props) {
+        super(props);
+        this.state = {
+            show_scoreadd: false
+        }
+    }
+
     render() {
         return (
             <div>
                 <h2 className="tf2-score mr-2">Score: { this.props.score }</h2>
-                <div className="tf2-score-add animated fadeIn">
-                    <h2 className="animated slideInUp">+1</h2>
-                </div>
+                <CSSTransition
+                    in={ this.state.show_scoreadd }
+                    classNames="scoreadd"
+                    timeout={ 500 }
+                    onEntered={ () => this.setState({ show_scoreadd: false }) }
+                    unmountOnExit>
+                    <h2 className="tf2-score-add">+1</h2>
+                </CSSTransition>
             </div>
         );
     }
--- src/index.js
+++ src/index.js
@@ -4,8 +4,8 @@ import ReactDOM from 'react-dom';
 import * as serviceWorker from './serviceWorker';
 
 import './css/index.css';
-import MainMenu from './MainMenu';
 import Dimmer from './Dimmer';
+import MainMenu from './MainMenu';
 
 ReactDOM.render(<Dimmer correct="false" />, document.getElementById('dimmers'));
 ReactDOM.render(<MainMenu />, document.getElementById('root'));
