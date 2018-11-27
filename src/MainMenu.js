190de1ff916ec1a95421ed1e300c8f79d0fa1b68
QwertygidQ <Qwertygid@gmail.com>
2018-11-28 01:10:35 +0300

    Changed Github to GitHub

--- src/MainMenu.js
+++ src/MainMenu.js
@@ -11,7 +11,7 @@ class MainMenu extends Component {
                 <img id="logo" src={ require("./img/logo.png") } alt="TF2 Item Quiz" />
                 <button type="button" className="btn btn-outline-light btn-block mt-5" onClick={ this.props.onGameStart }>Start the Quiz</button>
                 <form action="https://github.com/QwertygidQ/tf2-item-quiz" className="mt-2">
-                    <button type="submit" className="btn btn-outline-light btn-block">Github</button>
+                    <button type="submit" className="btn btn-outline-light btn-block">GitHub</button>
                 </form>
                 <button type="button" className="btn btn-outline-light btn-block mt-2" onClick={ this.props.onCredits }>Credits</button>
             </div>
